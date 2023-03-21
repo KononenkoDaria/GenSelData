648f329aa521f3fa2371cd85ac293f15ec0bd40d
KononenkoDaria <daria.kononenko.a@gmail.com>
2023-03-22 00:53:06 +0200

    New Project 0.0.1

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,16 @@ import Swiper, { Navigation, Pagination} from "swiper";
 
 const swiper = new Swiper();
 
+window.onload = function() {
+    var currentUrl = window.location.href;
+    var targetUrl = "http://localhost:8000/newProject.html";
+    
+    if (currentUrl === targetUrl) {
+      document.getElementById("close").style.display = "none";
+    }
+  };
+  
+
 //country list=======================
 
 /*const countrySelect = document.getElementById("countryList");
