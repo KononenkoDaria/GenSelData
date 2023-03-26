d08d1df5088bbd71079fc71fcc2b2e916ec87007
KononenkoDaria <daria.kononenko.a@gmail.com>
2023-03-26 18:48:57 +0300

    New Project 0.1.6

--- src/js/app.js
+++ src/js/app.js
@@ -14,12 +14,3 @@ masks();
 flsFunctions.isWebp();
 
 const swiper = new Swiper();
-
-window.onload = function() {
-    var currentUrl = window.location.href;
-    var targetUrl = "http://localhost:3000/newProject.html";
-    
-    if (currentUrl === targetUrl) {
-      document.getElementById("close").style.display = "none";
-    }
-};
