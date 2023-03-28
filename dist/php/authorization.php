420d1b5a42cc56cf61458c71f3e7693c4b8b67f7
OlPermiakova <olpermiakovaha@gmail.com>
2023-03-28 16:57:33 +0300

    php code

--- dist/php/authorization.php
+++ dist/php/authorization.php
@@ -1,4 +1,27 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "genseldata";
 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+
+$user_email = $_POST['user__email'];
+$user_password = md5($_POST['user__password']);
+$user_orcidID = preg_replace('/[^0-9]/', '', $_POST['user__orcidID']);
+
+    $result = $conn->query("SELECT * FROM user WHERE user_email = '$user_email' AND user_pass = '$user_password'");
+
+    If (mysqli_num_rows($result) > 0){
+        header('Location: ../mainAuthorizated.html');
+        setcookie('login', $user_email, time() + 3600, "/" );
+    }
+
+
+
+$conn->close();
 
 ?>
